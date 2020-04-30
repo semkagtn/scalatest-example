5ab4716769bb15c867858a9dbc59c754921c62a0
Nikita Bosov <neckbosov@gmail.com>
2020-04-30 16:58:49 +0300

    add lab

--- src/test/scala/com/semkagtn/extensions/MatcherSupport.scala
+++ src/test/scala/com/semkagtn/extensions/MatcherSupport.scala
@@ -7,19 +7,25 @@ trait MatcherSupport {
 
   // TODO generalize
   def greaterThan(value: Long): Matcher[Long] = new ScalatestMatcher[Long] {
-    
+
     override protected def matches(actualResult: Long): Boolean =
       actualResult > value
 
     override protected def asString: String =
       s"greater than $value"
   }
-  
+
   def equalTo[A](expected: A): Matcher[A] = new ScalatestMatcher[A] {
     override protected def matches(actualResult: A): Boolean =
       actualResult == expected
 
     override protected def asString: String =
       expected.toString
+  }
+
+  def approxEqual(expected: Double): Matcher[Double] = new ScalatestMatcher[Double] {
+    override protected def matches(actualResult: Double): Boolean = Math.abs(expected - actualResult) < 1e-6
+
+    override protected def asString: String = s"approximately equal to $expected"
   }
 }
